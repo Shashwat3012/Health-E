71d0037fa431c47a81d28b10986c9b321cff7f7a
ShwetaTripathi05 <1996shweta.tripathi@gmail.com>
2022-09-11 21:41:37 +0530

    Initial login page

--- frontend/health-e/src/app/app.module.ts
+++ frontend/health-e/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -18,9 +23,22 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    // MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+    // MatCardModule,
+    // MatSelectModule,
+    // MatRadioModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {floatLabel: 'auto'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
