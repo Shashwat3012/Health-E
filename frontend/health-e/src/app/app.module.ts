44d6a3b36c3c7c111e5ad54c4b471701dbd9f045
ShwetaTripathi05 <1996shweta.tripathi@gmail.com>
2022-09-11 18:40:38 +0530

    Login page

--- frontend/health-e/src/app/app.module.ts
+++ frontend/health-e/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login' }
+  //{ path: 'second-component', component: SecondComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
